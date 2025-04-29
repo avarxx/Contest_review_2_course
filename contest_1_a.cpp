f3ec0a484ab8a99202833ea1ec13c7fac39622c4
Anvar Valeev <anvarvaleev@MacBook-Air-Anvar.local>
2025-04-29 18:38:28 +0300

    fix

--- contest_1_a.cpp
+++ contest_1_a.cpp
@@ -1,68 +1,81 @@
 #include <iostream>
+#include <iterator>
+#include <string>
 #include <vector>
 
-std::vector<int> prefixFunction(const std::string& str) //взята четсно с презентации 
-{
-  std::vector<int> p(str.size(), 0);
-  for(size_t i = 1; i < str.size(); i++) 
-  {
-    int k = p[i - 1];
-    while (k > 0 && str[i] != str[k])
-    {
-      k = p[k - 1];
+std::vector<int> prefixFunction(const std::string& str) {
+    std::vector<int> p(str.size(), 0);
+    for (size_t i = 1; i < str.size(); ++i) {
+        int k = p[i - 1];
+        while (k > 0 && str[i] != str[k]) {
+            k = p[k - 1];
+        }
+        if (str[i] == str[k])
+            ++k;
+        p[i] = k;
     }
-    if (str[i] == str[k])
-    {
-      k++;
-    }
-    p[i] = k;
-  }
-  return p;
+    return p;
 }
 
-std::vector<int> kmp(const std::string& pattern, const std::string& text) 
-{
-  std::vector<int> p = prefixFunction(pattern);
-  int number_of_match = 0;
-  std::vector<int> result;
-  if (pattern.length() == 0) return result;
-  if (text.length() < pattern.length()) return result;
-
-  for (size_t i = 0; i < text.length(); i++)
-  {
-    while (number_of_match > 0 && pattern[number_of_match] != text[i])
-    {
-      number_of_match = p[number_of_match - 1];
+template <typename RA>
+std::vector<int> prefixFunction(RA first, RA last) {
+    using D = typename std::iterator_traits<RA>::difference_type;
+    D n = last - first;
+    std::vector<int> p(n, 0);
+    for (D i = 1; i < n; ++i) {
+        int k = p[i - 1];
+        while (k > 0 && first[i] != first[k]) {
+            k = p[k - 1];
+        }
+        if (first[i] == first[k])
+            ++k;
+        p[i] = k;
     }
-    if (pattern[number_of_match] == text[i])
-    {
-      number_of_match++;
-    }
-    if (number_of_match == pattern.length())
-    {
-      result.push_back(i - pattern.length() + 1);
-      number_of_match = p[number_of_match - 1];
-    }
-  }
-  return result;
+    return p;
 }
 
-void print(std::vector<int> result)
-{
-  for(size_t i = 0; i < result.size(); i++)
-  {
-    std::cout << result[i] << "\n";
-  } 
+template <typename PIt, typename TIt>
+std::vector<typename std::iterator_traits<TIt>::difference_type> kmp(PIt p0, PIt p1, TIt t0,
+                                                                     TIt t1) {
+    using D = typename std::iterator_traits<TIt>::difference_type;
+    D m = p1 - p0;
+    if (m == 0)
+        return {};
+    auto pi = prefixFunction(p0, p1);
+    std::vector<D> res;
+    D q = 0;
+    for (D i = 0; i < (t1 - t0); ++i) {
+        while (q > 0 && p0[q] != t0[i]) {
+            q = pi[q - 1];
+        }
+        if (p0[q] == t0[i])
+            ++q;
+        if (q == m) {
+            res.push_back(i - m + 1);
+            q = pi[q - 1];
+        }
+    }
+    return res;
 }
 
-int main(int argc, const char* argv[])
-{
-  std::string text, pattern; 
-  std::cin >> text >> pattern; 
+std::vector<int> kmp(const std::string& pattern, const std::string& text) {
+    auto tmp = kmp(pattern.begin(), pattern.end(), text.begin(), text.end());
+    return std::vector<int>(tmp.begin(), tmp.end());
+}
 
-  std::vector<int> result = kmp(pattern, text);
+void print(const std::vector<int>& result) {
+    for (int x : result) {
+        std::cout << x << "\n";
+    }
+}
 
-  print(result);
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 
-  return 0;
+    std::string text, pattern;
+    std::cin >> text >> pattern;
+    std::vector<int> result = kmp(pattern, text);
+    print(result);
+    return 0;
 }
