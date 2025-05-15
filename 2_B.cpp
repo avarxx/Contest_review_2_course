a42371a09705e12c7f456459f410fe38c8545726
Anvar Valeev <anvarvaleev@MacBook-Air-Anvar.local>
2025-05-15 23:07:35 +0300

    fix long long and size_t

--- 2_B.cpp
+++ 2_B.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <cstddef> 
 
-void compute_sum_of_lowest_prime_divisors(int n) {
-    std::vector<int> lowest_prime_divisor(n + 1, 0);
+void compute_sum_of_lowest_prime_divisors(std::size_t n) {
+    std::vector<std::size_t> lowest_prime_divisor(n + 1, 0);
 
-    for (int i = 2; i <= n; ++i) {
-        if (lowest_prime_divisor[i] == 0) { 
-            for (long long j = (long long)i * i; j <= n; j += i) {
+    for (std::size_t i = 2; i <= n; ++i) {
+        if (lowest_prime_divisor[i] == 0) {
+            for (std::size_t j = i * i; j <= n; j += i) {
                 if (lowest_prime_divisor[j] == 0) {
                     lowest_prime_divisor[j] = i;
                 }
             }
-            lowest_prime_divisor[i] = i; 
+            lowest_prime_divisor[i] = i;
         }
     }
 
-    long long sum_of_lowest_prime_divisors = 0;
-    for (int i = 3; i <= n; ++i) {
-        if (lowest_prime_divisor[i] != i) { 
+    unsigned long long sum_of_lowest_prime_divisors = 0;
+    for (std::size_t i = 3; i <= n; ++i) {
+        if (lowest_prime_divisor[i] != i) {
             sum_of_lowest_prime_divisors += lowest_prime_divisor[i];
         }
     }
@@ -26,7 +27,7 @@ void compute_sum_of_lowest_prime_divisors(int n) {
 }
 
 int main() {
-    int n;
+    std::size_t n;
     std::cin >> n;
     compute_sum_of_lowest_prime_divisors(n);
     return 0;
